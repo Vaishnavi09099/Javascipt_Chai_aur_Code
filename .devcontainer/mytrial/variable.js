0b8fe80e3d6212f67a3890ef58cf13aa3d9c2431
Vaishnavi09099 <Vaishnavi09.099@gmail.com>
2025-05-09 13:35:45 +0000

    datatypes

---
+++ .devcontainer/mytrial/variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "vashnavi@google.com"
+var accountPass = "aashtiu"
+accountCity = "Jaipur"
+accountEmail = "hc@hc.com"
+accountPass = "545669"
+accountCity = "Orai"
+//accountId = 2 not allowed
+
+/* 
+Prefer not to use var because of issue in block scope and functional scope
+*/
+let ok;
+console.log(ok);
+
+
