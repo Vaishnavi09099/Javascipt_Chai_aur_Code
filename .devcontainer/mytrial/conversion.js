3a903ca2b30463686a6713841f617d982fa9a6b1
Vaishnavi09099 <Vaishnavi09.099@gmail.com>
2025-05-09 13:56:42 +0000

    conversion of datatypes

---
+++ .devcontainer/mytrial/conversion.js
@@ -0,0 +1,31 @@
+// let score = "hitesh"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// // console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+
+// let isLoggedIn = "vaishu"
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// 1 =>true
+//0 => false
+// "" empty string =>false
+// name = true  eg. "vaishnavi"
+
+let some = 33
+let strnum = String(some)
+console.log(typeof strnum);
+
+console.log(strnum);
+
