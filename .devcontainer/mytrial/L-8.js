771812a0c6802ea0460d3cfa918ac4fa9fd43414
Vaishnavi09099 <Vaishnavi09.099@gmail.com>
2025-05-10 03:57:59 +0000

    Datatypes

---
+++ .devcontainer/mytrial/L-8.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >=0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+//avoid above types of conversions always use cleancode
+
+//===
+console.log("2" === 2);
+
+
+
+
---
+++ .devcontainer/mytrial/L-9.js
@@ -0,0 +1,28 @@
+// #PRIMITIVE DATA TYPES(Call by value)
+// 7 types : String, Number, Boolean, null,undefined,,Symbol, BigInt
+
+
+//#REFERENCE TYPE(Non-Primitive)
+//Arrays, Objects,Functions
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+const bigNumber = 555555555555n
+
+
+// const heroes = ["shaktiman","naagraj" , "doga"]
+// let myObj = {
+//     name: "vaishnavi",
+//     age: 22,
+
+// }
+
+const myfunction = function(){
+    console.log("Hello world");
+    
+
+}
+console.log(typeof bigNumber);
+console.log(typeof myfunction);
+
+
